9a1a868abcef0bd5ad2c12b7d04409a0e617e9af
Paulo Caetano <paulocaetanomt88@gmail.com>
2023-02-20 21:56:19 -0400

    Criando a Model Pagamento

---
+++ Api_cartao/Models/Pagamento.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_cartao.Models
+{
+    public class Pagamento
+    {
+        public int PagamentoId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string NomeTitular { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string NumeroCartao { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string DataExpiracao { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CVV { get; set; }
+    }
+}
